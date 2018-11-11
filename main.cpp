33b72520b443b54992911687f1b9e5b1fda760d1
Armando Gutierrez <armandogutierrez@ucrwpa-1-6-10-25-21-31.wnet.ucr.edu>
2018-11-11 10:04:24 -0800

    Created main.cpp

---
+++ main.cpp
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: armandogutierrez
+ *
+ * Created on November 11, 2018, 9:42 AM
+ */
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+void inputNumbers(const string &s);
+
+int main(int argc, char** argv) {
+    
+    
+    
+    return 0;
+}
+
