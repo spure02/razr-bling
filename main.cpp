3198957d91304c6e38737064b7b309f02781790c
armandogutierrez4802 <armandogutierrez4802@gmail.com>
2018-11-11 11:09:22 -0800

    Add files via upload
    variables

--- main.cpp
+++ main.cpp
@@ -2,17 +2,22 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-
+#include <stack>
+#include <vector>
 using namespace std;
 
 void inputNumbers(const string &s);
 
 int main(int argc, char** argv) {
-    string wordInput;
+    string input;//string of integers
     cout << "Enter word" << endl;
-    cin >> wordInput;
+    cin >> input;
+    int stringLength = input.length();
+    char queue[stringLength];
+    vector<string> vec;
+    
+    cout << stringLength << endl;
     
     
     return 0;
 }
-
